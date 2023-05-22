2eb185a50d5c86cc76ee72d57007b90f751a7e09
Teik Guan Tan <tanteikg@gmail.com>
2023-05-22 22:52:43 +0800

    update

--- _next/HS2ioUTSXDxYLMKe9hagr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/158-788d96623fb444ce.js","static/chunks/pages/index-9b40564f136e45e5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
